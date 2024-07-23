34b61c30180418718f47ca0b978e9bb2cb0241fc
Takuya Matsuyama <hi@craftz.dog>
2024-07-23 15:01:44 +0900

    fix(tokens-tags): make it work on React Native

--- task-progress.ts
+++ task-progress.ts
@@ -1,15 +1,15 @@
 import { hsl } from './tokens'
 
 export const taskProgress = {
-  borderColor: `hsl(${hsl.gray950} / 30%)`,
-  backgroundColor: `hsl(${hsl.gray950} / 30%)`,
-  foregroundColor: `hsl(${hsl.white} / 60%)`,
+  borderColor: `hsla(${hsl.gray950} / 0.3)`,
+  backgroundColor: `hsla(${hsl.gray950} / 0.3)`,
+  foregroundColor: `hsla(${hsl.white} / 0.6)`,
   completedColor: `hsl(${hsl.green500})`
 }
 
 export const taskProgressDark = {
-  borderColor: `hsl(${hsl.gray50} / 10%)`,
-  backgroundColor: `hsl(${hsl.gray950} / 30%)`,
-  foregroundColor: `hsl(${hsl.gray50} / 60%)`,
+  borderColor: `hsla(${hsl.gray50} / 0.1)`,
+  backgroundColor: `hsla(${hsl.gray950} / 0.3)`,
+  foregroundColor: `hsla(${hsl.gray50} / 0.6)`,
   completedColor: `hsl(${hsl.green500})`
 }
